d48b7dae8d7cb15a6006d09caf3d6fe13662224c
frotaur <frotaur@hotmail.co.uk>
2021-09-22 18:13:00 +0200

    Scraping work with yandex

--- whale_scraping/whale_scrape.py
+++ whale_scraping/whale_scrape.py
@@ -1,27 +1,67 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 from bs4 import BeautifulSoup
+import requests
+import pickle
+
 driver = webdriver.Chrome("chromedriver")
+driver.maximize_window()
+driver.get("https://yandex.com/images/search?text=whale")
+actionman = ActionChains(driver)
 
-driver.get("https://www.google.com/search?q=whale&tbm=isch&ved\
-	=2ahUKEwipntyzkpDzAhUX8IUKHRyYCgwQ2-cCegQIABAA&oq=whale&gs_\
-	lcp=CgNpbWcQA1AAWABg1wxoAHAAeACAAQCIAQCSAQCYAQCqAQtnd3Mtd2l6\
-	LWltZw&sclient=img&ei=P9pJYemCLZfglwScsKpg&bih=1057&biw=1920")
-
-time.sleep(2)
+time.sleep(10)
 scroll_pause = 1
 screen_height = driver.execute_script("return window.screen.height;")
 i=1
 scroll_max=10
+last_height=driver.execute_script("return document.body.scrollHeight")
 
-while True:
-	driver.execute_script("window.scrollTo(0,{screen_h}*{i});".format(screen_h=screen_height,i=i))
-	i+=1
+while i<scroll_max:
+	# Scroll down to bottom
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+	# Wait to load page
 	time.sleep(scroll_pause)
-	scroll_height = driver.execute_script("return document.body.scrollHeight;")
-	if (screen_height * i > scroll_height or i> scroll_max):
-		break  
 
-img_urls = []
+	# Calculate new scroll height and compare with last scroll height
+	new_height = driver.execute_script("return document.body.scrollHeight")
+
+	if new_height == last_height:
+	#We reached the end
+		break
+
+	last_height = new_height
+	i+=1
+
+time.sleep(10)
+
+image_links = driver.find_elements_by_class_name("serp-item__link")
+
+actionman.move_to_element(image_links[0]).perform()
+image_links[0].click()
+time.sleep(1)
+
+
+url_tab= []
+ended=False
+while not ended :
+	time.sleep(1)
+	url_temp = driver.find_elements_by_class_name("MMImage-Origin")
+	print(url_temp[0].get_attribute("src"))
+	url_temp[0].click()
+	if(url_tab):
+		if(url_tab[-1]==url_temp[0].get_attribute("src")):
+			ended=True
+	url_tab.append(url_temp[0].get_attribute("src"))
+
+
+
+# Open a file and use dump()
+with open('whale-url.pkl', 'wb') as file:
+      
+    # A new file will be created
+    pickle.dump(url_tab, file)
 
 driver.close()
+driver.quit()
